60f83474000d924169d7aa8d003b2282c2a94a31
wealthczarena <98209911+wealthczarena@users.noreply.github.com>
2023-08-24 01:45:48 -0300

    Update fun.js

--- vite-project/fun.js
+++ vite-project/fun.js
@@ -12,9 +12,6 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 camera.position.z = 5;
-// This was one of reasons for the bug. You are not seeing the cube because the camera is at the same position as the cube by default, i.e., at (0, 0, 0.
-// This is a common mistake when starting with Three.js. All the objects dragged at the scene are at (0, 0, 0) by default.
-// So, if you don't move the camera, you won't see anything becaue they are overlapping each other.
 
 // Setting up a renderer
 const renderer = new THREE.WebGLRenderer();
@@ -38,16 +35,11 @@ scene.add(sphereMesh);
 const light = new THREE.AmbientLight(0x404040); // soft white light
 scene.add(light);
 
-// render the scene. This is the second reason why you are not seeing the cube. You are not rendering the scene.
-// The function animate is called over and over again, once per frame.
-// Once per frame means, as fast as possible. Usually, this is 60 frames per second, but it depends on the browser and the device.
-// Meaning, if you want to animate something, you have to change the properties of the cube inside the animate function.
 function animate() {
-  requestAnimationFrame(animate); // requestAnimationFrame is a function that tells the browser that you wish to perform an animation and requests that the browser calls a specified function to update an animation before the next repaint.
-  cubeMesh.rotation.x += 0.01; // here we are changing the rotation of the cube on the x-axis on every frame by 0.01, to make it spin.
-  cubeMesh.rotation.y += 0.01; // same for the y axis.
+  requestAnimationFrame(animate);
+  cubeMesh.rotation.y += 0.01; .
   sphereMesh.rotation.x += 0.01;
   sphereMesh.rotation.y += 0.01;
-  renderer.render(scene, camera); // render the scene. This will create one still image / frame of the scene. It is like a snapshot of the camera. The "render" function takes two arguments: the scene and the camera.
+  renderer.render(scene, camera); 
 }
-animate(); // call the animate function. This will start the animation loop.
+animate(); 
